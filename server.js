66b91595c1c00fdfc1fee81ee157ec2cea0e39e5
BOACPLTD <praburamm28@gmail.com>
2024-08-02 17:49:11 +0530

    add

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.post('/verify-otp', (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
